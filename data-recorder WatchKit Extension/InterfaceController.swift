6fb05983313001e27a17c559f9ea27b9ea2507a2
JoaoFE <joaoeirinh@gmail.com>
2019-05-01 13:43:57 -0400

    Initial Commit

---
+++ data-recorder WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  data-recorder WatchKit Extension
+//
+//  Created by XCodeClub on 2019-05-01.
+//  Copyright © 2019 dataset-devteam. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
